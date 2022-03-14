95af8aa88ca46549d3959a879f6ab1c697b0be03
matheusfillipe <matheusfillipeag@gmail.com>
2022-03-14 03:21:40 -0300

    Circle CI heroku deploy

---
+++ .circleci/deploy-heroku.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+commit_hash=$(git rev-parse HEAD)
+cd build/html
+heroku git:remote -a $HEROKU_APP_NAME
+echo "{}" > composer.json
+echo "<?php include_once(\"index.html\"); ?>" > index.php
+git add .
+git commit -am "$commit_hash"
+git push heroku master
---
+++ .circleci/setup-heroku.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
+
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+EOF
+
+git clone https://git.heroku.com/$HEROKU_APP_NAME.git build/html
