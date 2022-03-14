01cbd29406bed7a1878121a7727880df0a33c27b
matheusfillipe <matheusfillipeag@gmail.com>
2022-03-14 03:46:08 -0300

    Heroku 6

--- .circleci/deploy-heroku.sh
+++ .circleci/deploy-heroku.sh
@@ -2,6 +2,8 @@
 
 commit_hash=$(git rev-parse HEAD)
 cd build/html
+git config --global user.email "$HEROKU_LOGIN"
+git config --global user.name "CircleCI"
 heroku git:remote -a $HEROKU_APP_NAME
 echo "{}" > composer.json
 echo "<?php include_once(\"index.html\"); ?>" > index.php
