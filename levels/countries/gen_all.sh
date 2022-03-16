07f1dece9c245baa6d316d915935db2085ce8bd3
matheusfillipe <matheusfillipeag@gmail.com>
2022-03-16 17:22:12 -0300

    Geography mode!
    Some minimal bugfixes with the z index
    Bugfixes with randomness
    Minimal geography mode

--- levels/countries/gen_all.sh
+++ levels/countries/gen_all.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if ! command -v convert >/dev/null 2>&1; then
+    echo "convert command not found"
+    exit 1
+fi
 pip install -r requirements.txt
 ./download_countries_json.sh
 ./download_maps.py
